d1d7678d88914752ab8f2c6b8b7cc961f1f7ed15
russellmendonca <rmendonc@andrew.cmu.edu>
2021-05-12 17:21:08 -0400

    changed mocap weld position and implemented 3dof control

--- d4rl/kitchen/run_kitchen.py
+++ d4rl/kitchen/run_kitchen.py
@@ -1,21 +1,20 @@
 import gym
 import d4rl
 import cv2
-from envs.d4rl_envs import KitchenEnv
-#from d4rl.kitchen.kitchen_envs import KitchenMicrowaveV0
+#from envs.d4rl_envs import KitchenEnv
+from d4rl.kitchen.kitchen_envs import KitchenMicrowaveV0
 import imageio
+from matplotlib import pyplot as plt
 
-env = KitchenEnv()
-env.reset()
-done = False
-imgs = []
-for i in range(150):
-    o, r, d, i = env.step(env.action_space.sample())
-    print("microwave", i["microwave distance to goal"])
-    im = env.render(mode="rgb_array")
-    imgs.append(im)
-    #cv2.imshow("env", im)
-    #cv2.waitKey(1)
-    #print(r)
-#import ipdb ; ipdb.set_trace()
-imageio.mimsave('out.gif', imgs)
+for ee_control_type in ['6dof', '3dof_gripper_rot', '3dof']:
+    env = KitchenMicrowaveV0(control_mode='end_effector', ee_control_type= ee_control_type)
+
+    env.reset()
+    done = False
+    imgs = []
+    for i in range(150):
+        a = env.action_space.sample()
+        im = env.render(mode="rgb_array")
+        o, r, d, i = env.step(a)
+        imgs.append(im)
+    imageio.mimsave('ctrl_type_'+str(ee_control_type)+'.gif', imgs)
